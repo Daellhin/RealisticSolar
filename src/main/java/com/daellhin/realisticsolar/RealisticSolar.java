dda15de462558aa02efe7ee9db451391ede90914
Daellhin <31322750+Daellhin@users.noreply.github.com>
2019-03-04 10:53:12 +0100

    Adding items

--- src/main/java/com/daellhin/realisticsolar/RealisticSolar.java
+++ src/main/java/com/daellhin/realisticsolar/RealisticSolar.java
@@ -3,7 +3,14 @@ package com.daellhin.realisticsolar;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.daellhin.realisticsolar.list.ItemList;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -33,5 +40,24 @@ public class RealisticSolar {
 	private void clientRegistries(final FMLClientSetupEvent event) {
 		logger.info("ClientRegistries method registerd");
 	
+	}
+	
+	@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents{
+		@SubscribeEvent
+		public static void registerItems(final RegistryEvent.Register<Item> event){
+			event.getRegistry().registerAll(
+			
+			ItemList.tutorial_item = new Item(new Item.Properties().group(ItemGroup.COMBAT)).setRegistryName(location("tutorial_item"))
+			);
+			
+			logger.info("Items registered");
+			
+		}
+		private static ResourceLocation location(String name) {
+			return new ResourceLocation(modid, name);
+			
+		}
+		
 	}
 }
---
+++ src/main/java/com/daellhin/realisticsolar/list/ItemList.java
@@ -0,0 +1,8 @@
+package com.daellhin.realisticsolar.list;
+
+import net.minecraft.item.Item;
+
+public class ItemList {
+	public static Item tutorial_item;
+
+}
