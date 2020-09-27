74b99fce984b36c1fc8f76cf474ca100150d1a1e
Daellhin <>
2020-09-27 22:28:07 +0200

    Fixed custom voxel shapes not showing, generalised with builder

--- src/main/java/com/daellhin/realisticsolar/blocks/siemensreactor/SiemensReactorBottomBlock.java
+++ src/main/java/com/daellhin/realisticsolar/blocks/siemensreactor/SiemensReactorBottomBlock.java
@@ -4,11 +4,6 @@ import com.daellhin.realisticsolar.blocks.base.BaseBlock;
 import com.daellhin.realisticsolar.tools.BlockBuilder;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.shapes.ISelectionContext;
-import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.world.IBlockReader;
 
 public class SiemensReactorBottomBlock extends BaseBlock {
 
@@ -16,15 +11,9 @@ public class SiemensReactorBottomBlock extends BaseBlock {
 	// TODO check if block is formed +(maybe change facing according to other block)
 	// public static final BooleanProperty FORMED =
 	// BooleanProperty.create("formed");
-	protected static final VoxelShape SHAPE = Block.makeCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D);
 
 	public SiemensReactorBottomBlock() {
-		super(new BlockBuilder().basicProperties().tileEntitySupplier(SiemensReactorTile::new));
-	}
-
-	@Override
-	public VoxelShape getShape(BlockState state, IBlockReader iBlockReader, BlockPos pos, ISelectionContext selectionContext) {
-		return SHAPE;
+		super(new BlockBuilder().basicProperties().tileEntitySupplier(SiemensReactorTile::new).shape(Block.makeCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D)));
 	}
 
 }
--- src/main/java/com/daellhin/realisticsolar/blocks/siemensreactor/SiemensReactorTopBlock.java
+++ src/main/java/com/daellhin/realisticsolar/blocks/siemensreactor/SiemensReactorTopBlock.java
@@ -4,11 +4,6 @@ import com.daellhin.realisticsolar.blocks.base.BaseBlock;
 import com.daellhin.realisticsolar.tools.BlockBuilder;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.shapes.ISelectionContext;
-import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.world.IBlockReader;
 
 public class SiemensReactorTopBlock extends BaseBlock {
 
@@ -16,22 +11,9 @@ public class SiemensReactorTopBlock extends BaseBlock {
 	// TODO check if block is formed +(maybe change facing according to other block)
 	// public static final BooleanProperty FORMED =
 	// BooleanProperty.create("formed");
-	protected static final VoxelShape SHAPE = Block.makeCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D);
 
 	public SiemensReactorTopBlock() {
-		super(new BlockBuilder().basicProperties());
+		super(new BlockBuilder().basicProperties().shape(Block.makeCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D)));
 	}
-
-	@Override
-	public VoxelShape getShape(BlockState state, IBlockReader iBlockReader, BlockPos pos, ISelectionContext selectionContext) {
-		return SHAPE;
-	}
-
-// TODO get the tileEntity of the bottom reactor
-//    @Nullable
-//    @Override
-//    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
-//	return new SiemensReactorTile();
-//    }
 
 }
