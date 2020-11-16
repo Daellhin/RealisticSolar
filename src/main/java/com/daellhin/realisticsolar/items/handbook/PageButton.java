5a342e807eed1668751b0ae7a7f03952da53c690
Daellhin <lorin.speybrouck@proximus.be>
2020-11-16 22:24:15 +0100

    Simplified PageButton

--- src/main/java/com/daellhin/realisticsolar/items/handbook/PageButton.java
+++ src/main/java/com/daellhin/realisticsolar/items/handbook/PageButton.java
@@ -15,27 +15,19 @@ public class PageButton extends Button {
 	private final ResourceLocation resourceLocation;
 	private int textureX;
 	private int textureY;
-	private final int buttonWidth;
-	private final int buttonHeight;
-	private final int xPos;
-	private final int yPos;
 
 	public PageButton(int xPos, int yPos, ResourceLocation resourceLocation, int textureX, int textureY, int buttonWidth, int buttonHeight, IPressable onPress) {
 		super(xPos, yPos, buttonWidth, buttonHeight, "", onPress);
-		this.xPos = xPos;
-		this.yPos = yPos;
 		this.resourceLocation = resourceLocation;
 		this.textureX = textureX;
 		this.textureY = textureY;
-		this.buttonWidth = buttonWidth;
-		this.buttonHeight = buttonHeight;
 	}
 
 	@Override
-	public void renderButton(int p_renderButton_1_, int p_renderButton_2_, float p_renderButton_3_) {
+	public void renderButton(int mouseX, int mouseY, float partialTicks) {
 		Minecraft.getInstance()
 				.getTextureManager()
 				.bindTexture(resourceLocation);
-		blit(xPos, yPos, textureX + (this.isHovered ? buttonWidth : 0), textureY, buttonWidth, buttonHeight, 64, 64);
+		blit(this.x, this.y, textureX + (this.isHovered ? this.width : 0), textureY, this.width, this.height, 64, 64);
 	}
 }
